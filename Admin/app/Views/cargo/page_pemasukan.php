18dd319738373c9569cc109311b612b37b18fdf2
IndraSbudi <128012203+IndraSbudi@users.noreply.github.com>
2024-07-02 01:37:44 +0700

    all

--- Admin/app/Views/cargo/page_pemasukan.php
+++ Admin/app/Views/cargo/page_pemasukan.php
@@ -98,7 +98,7 @@
                             <div class="col-12">
                                 <div class="card">
                                     <div class="card-body">
-                                        <table id="main_table" class="table table-bordered nowrap w-100">
+                                        <table id="main_table" class="table table-bordered">
                                             <thead>
                                                 <tr class="text-center">
                                                     <th>No</th>
