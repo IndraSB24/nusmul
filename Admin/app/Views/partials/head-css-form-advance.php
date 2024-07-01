7bd90f3f35f73240d89ac3ba89fba4ad4c753bff
IndraSbudi <128012203+IndraSbudi@users.noreply.github.com>
2024-07-02 02:22:32 +0700

    all

--- Admin/app/Views/partials/head-css-form-advance.php
+++ Admin/app/Views/partials/head-css-form-advance.php
@@ -1,10 +1,10 @@
 <!-- choices css -->
-<link href="assets/libs/choices.js/public/assets/styles/choices.min.css" rel="stylesheet" type="text/css" />
+<link href="<?= base_url('assets/libs/choices.js/public/assets/styles/choices.min.css'); ?>" rel="stylesheet" type="text/css" />
 
 <!-- color picker css -->
-<link rel="stylesheet" href="assets/libs/@simonwep/pickr/themes/classic.min.css" /> <!-- 'classic' theme -->
-<link rel="stylesheet" href="assets/libs/@simonwep/pickr/themes/monolith.min.css" /> <!-- 'monolith' theme -->
-<link rel="stylesheet" href="assets/libs/@simonwep/pickr/themes/nano.min.css" /> <!-- 'nano' theme -->
+<link rel="stylesheet" href="<?= base_url('assets/libs/@simonwep/pickr/themes/classic.min.css'); ?>" /> <!-- 'classic' theme -->
+<link rel="stylesheet" href="<?= base_url('assets/libs/@simonwep/pickr/themes/monolith.min.css'); ?>" /> <!-- 'monolith' theme -->
+<link rel="stylesheet" href="<?= base_url('assets/libs/@simonwep/pickr/themes/nano.min.css'); ?>" /> <!-- 'nano' theme -->
 
 <!-- datepicker css -->
-<link rel="stylesheet" href="assets/libs/flatpickr/flatpickr.min.css">
+<link rel="stylesheet" href="<?= base_url('assets/libs/flatpickr/flatpickr.min.css'); ?>">
