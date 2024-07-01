10632a7bc61ce67d898e9d4ebafc3eeec2798b9d
IndraSbudi <128012203+IndraSbudi@users.noreply.github.com>
2024-07-02 02:19:57 +0700

    all

--- Admin/app/Views/cargo/page_pemasukan.php
+++ Admin/app/Views/cargo/page_pemasukan.php
@@ -290,18 +290,6 @@
         <?= $this->include('partials/vendor-scripts') ?>
         <?= $this->include('partials/vendor-scripts-sweetalert') ?>
         <?= $this->include('partials/vendor-scripts-datatable') ?>
-        <!-- choices js -->
-        <script src="assets/libs/choices.js/public/assets/scripts/choices.min.js"></script>
-
-        <!-- color picker js -->
-        <script src="assets/libs/@simonwep/pickr/pickr.min.js"></script>
-        <script src="assets/libs/@simonwep/pickr/pickr.es5.min.js"></script>
-
-        <!-- datepicker js -->
-        <script src="assets/libs/flatpickr/flatpickr.min.js"></script>
-
-        <!-- init js -->
-        <script src="assets/js/pages/form-advanced.init.js"></script>
         <?= $this->include('partials/custom-script') ?>
 
         <script src="assets/js/app.js"></script>
